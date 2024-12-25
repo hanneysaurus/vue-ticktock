3c038a876b50b8f8edb2ce2b3fda350af40c3846
Hannah <hanneysaurus@hotmail.com>
2024-12-25 17:09:07 +0100

    text styling

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <h1 style="font-family: Harrington,serif; color: white">let's party!</h1>
-  <h2 style="font-family: 'Bookman Old Style', Serif; color: gold;">Tick...Tack...BUMM</h2>
-  <h2 style="font-family: 'Bookman Old Style', Serif; color: gold; font-weight: bold">ACTIVITY</h2>
+  <h1 style="font-family: Harrington,serif; color: white; text-align: start">let's party!</h1>
+  <h2 style="font-family: 'Bookman Old Style', Serif; color: gold; text-align: center">Tick...Tack...BUMM</h2>
+  <h2 style="font-family: 'Bookman Old Style', Serif; color: gold; font-weight: bold; text-align: end">ACTIVITY</h2>
   <Bomb></Bomb>
 </template>
 
