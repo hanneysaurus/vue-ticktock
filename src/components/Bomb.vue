55453e1ebf6feda3a53e07b4e7b953855b302ff9
Hannah <hanneysaurus@hotmail.com>
2024-12-25 16:04:56 +0100

    added stopping function

--- src/components/Bomb.vue
+++ src/components/Bomb.vue
@@ -1,6 +1,6 @@
 <template>
   <img
-      src="../assets/bomb.png"
+      :src="imagePath"
       alt="bomb"/>
   <button ref="button" @click="handleClick">{{ isTicking ? 'STOP' : 'START' }}</button>
 </template>
@@ -12,32 +12,30 @@ import {ref} from "vue";
 const isTicking = ref(false);
 const initialPlaybackrate = 1.0;
 const finalPlaybackRate = 2.0;
+const imagePath = ref('/bomb.png');
 let time = 0;
 
 const tickingAudio = new Audio('/ticking.mp3');
 const explosionAudio = new Audio('/explosion.mp3');
 
 const handleClick = () => {
-  console.log("IS TICKING "+ isTicking.value);
   if (isTicking.value) {
     endBomb();
-    console.log("ending bomb");
   } else {
     startBomb();
-    console.log("starting bomb");
   }
 }
 
 const startBomb = () => {
 
-  isTicking.value = true;
+  imagePath.value = '/bomb.png';
 
   // times in seconds
   const minimumTime = 15;
   const maximumTime = 45;
   time = Math.floor(Math.random() * (maximumTime - minimumTime + 1)) + minimumTime;
 
-  let playbackRate = 0.9;
+  let playbackRate = initialPlaybackrate;
   let playbackRateIncrease = (finalPlaybackRate / (time));
 
   tickingAudio.loop = true;
@@ -48,18 +46,21 @@ const startBomb = () => {
     tickingAudio.playbackRate = playbackRate;
   }, 1000);
 
-  endBomb();
+  isTicking.value = true;
+
+  setTimeout(async () => {
+    await explosionAudio.play();
+    imagePath.value = '/explosion.png';
+    endBomb();
+  }, 1000 * time);
+
 }
 
 const endBomb = () => {
+  tickingAudio.pause();
+  tickingAudio.currentTime = 0;
+  tickingAudio.playbackRate = initialPlaybackrate;
   isTicking.value = false;
-
-  setTimeout(() => {
-    tickingAudio.pause();
-    tickingAudio.currentTime = 0;
-    tickingAudio.playbackRate = initialPlaybackrate;
-    explosionAudio.play();
-  }, 1000 * time);
 }
 
 </script>
